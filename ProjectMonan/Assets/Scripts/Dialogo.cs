b61ac8f58dbeafa614d74cb71a0cf7e9c3e72fed
Bartô Alheiros <alheirosb@gmail.com>
2020-10-11 23:25:41 -0300

    Caixa de Diálogo e Scripts
    Adicionada Caixa de Diálogo e alguns scripts soltos.

--- ProjectMonan/Assets/Scripts/Dialogo.cs
+++ ProjectMonan/Assets/Scripts/Dialogo.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Dialogo : MonoBehaviour
 {
+    public FalaNPC[] falas = new FalaNPC[2];
+    private bool dialogoConcluido = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,24 @@ public class Dialogo : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // método responsável por 'disparar' o diálogo
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerController>().Move(0);
+
+            if(!dialogoConcluido) 
+            {
+
+            } else 
+            {
+
+            }
+
+            dialogoConcluido = true;
+        }
     }
 }
