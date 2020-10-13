ca4f0d5d2a79eeb7a475283e5a6a1a39bc433b14
Bartô Alheiros <alheirosb@gmail.com>
2020-10-13 19:41:24 -0300

    Sistema de Diálogo Criado
    Agora será preciso modificar a dinâmica das falas, para ficar de acordo com o jogo.

---
+++ ProjectMonan/Assets/Scripts/Dialogo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogo : MonoBehaviour
+{
+    public FalaNPC[] falas = new FalaNPC[2];
+    private bool dialogoConcluido = false;
+    DialogoController dialogoController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogoController = FindObjectOfType<DialogoController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // método responsável por 'disparar' o diálogo
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+      
+        if(other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerController>().Move(0);
+
+            if(!dialogoConcluido) 
+            {
+                dialogoController.ProximaFala(falas[0]);
+            } 
+            else 
+            {
+                dialogoController.ProximaFala(falas[1]);
+            }
+
+            dialogoConcluido = true;
+        }
+    }
+}
