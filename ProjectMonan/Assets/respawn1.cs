6e1293488a1f25dfa1803c1bce733361722a1f4c
Lucas Sgotti <lsgotti@hotmail.com>
2020-10-20 18:07:41 -0300

    Respawn do buraco
    Respawn simples quando cai no buraco (to com dúvida na hora de tirar a vida do personagem) troquei o material do personagem pra ele não ficar prendendo nas paredes

---
+++ ProjectMonan/Assets/respawn1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn1 : MonoBehaviour {
+    public GameObject player;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        player.transform.position = new Vector3(40, 0, 0);
+    }   
+
+}
---
+++ ProjectMonan/Assets/respawn2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn2 : MonoBehaviour
+{
+    public GameObject player;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        player.transform.position = new Vector3(189, 18, 0);
+    }
+
+}
