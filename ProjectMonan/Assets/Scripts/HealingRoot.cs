53823feb8dd6c6594fc6099c88862b13c005f061
Lucas Sgotti <lsgotti@hotmail.com>
2020-10-22 14:10:40 -0300

    Planta com Heal
    Coloquei uma planta com heal e concertei alguns dos scripts relativos a barra de vida
    (update com o master)

---
+++ ProjectMonan/Assets/Scripts/HealingRoot.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HealingRoot : MonoBehaviour
+{
+    public bool isPressed;
+    public GameObject keySprite;
+    public int HealthPool = 50;
+    public GameObject player;
+    public GameObject animSprite;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (HealthPool > 0)
+        {
+            keySprite.active = true;
+        }
+    }
+
+
+    void Update()
+    {
+
+        if (keySprite.active)
+        {
+            if (Input.GetButtonDown("Prompt"))
+            {
+                if (HealthPool > 0)
+                {
+                    if (player.GetComponent<PlayerHealth>().maxHealth > player.GetComponent<PlayerHealth>().currentHealth)
+                    {
+                        HealthPool -= 1;
+                        player.GetComponent<PlayerHealth>().Heal(1);
+                    }
+                }
+            }
+        }
+        if (HealthPool == 0)
+        {
+            animSprite.GetComponent<Animator>().Play("rootColor");
+            keySprite.active = false;
+        }
+
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        keySprite.active = false;
+    }
+}
