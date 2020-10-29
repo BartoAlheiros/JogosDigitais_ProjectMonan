80c5afb5864cee3183dcc6058df446f1d088fb1e
Bartô Alheiros <alheirosb@gmail.com>
2020-10-28 23:05:01 -0300

    Script EnemyController
    Para ser usado através de herança, em qualquer inimigo.

---
+++ ProjectMonan/Assets/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    protected int heatlh;
+    public int speed;
+    protected bool isMoving = false;
+
+    protected Rigidbody2D rb;
+    protected Animator anim;
+    protected Transform player;
+    protected SpriteRenderer sprite;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        player = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
